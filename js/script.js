96d3fb1878eda09d590fef1faa32009112843c2d
SrCosmicCat <gatitosplus000@gmail.com>
2022-11-04 09:08:38 -0600

    API user functions

--- js/script.js
+++ js/script.js
@@ -1,56 +1,57 @@
 $(document).ready( () => {
-    function mostrarUsuarios() {
+    function showUsers() {
         codigoHTML = '';
 
         $.ajax({
-            url: 'http://localhost:3000/api/mostrarUsuarios',
+            url: 'http://localhost:3000/api/mostrarUsuarios', //TODO Cambiar Ruta de la API
             type: 'GET',
-            success: (contactos) => {
-                contactos.forEach((contacto) => {
-                    contacto.foto == null ? contacto.foto += ".png" : contacto.foto = contacto.foto;
+            success: (users) => {
+                users.forEach((user) => {
+                    user.foto == null ? user.foto += ".png" : user.foto = user.foto;
                     
                     codigoHTML += `
                         <tr>
-                            <td>${contacto.id}</td>
-                            <td>${contacto.nombre}</td>
-                            <td>${contacto.correo}</td>
-                            <td>${contacto.password}</td>
-                            <td><img src="http://localhost:3000/web/img/users/${contacto.foto}" width="100rem" alt=""></td>
+                            <td>${user.id}</td>
+                            <td>${user.usr}</td>
+                            <td>${user.name}</td>
+                            <td>${user.email}</td>
+                            <td>${user.password}</td>
+                            <td><img src="http://localhost:3000/web/img/users/${user.photo}" width="100rem" alt=""></td> //TODO Cambiar Ruta de Fotos
                             <td>
-                                <a href="#" class="modificar" id="${contacto.id}">Editar</a>
-                                <a href="#" class="borrar" id="${contacto.id}">Borrar</a>
+                                <a href="#" class="modificar" id="${user.id}">Editar</a>
+                                <a href="#" class="borrar" id="${user.id}">Borrar</a>
                             </td>
                         </tr>
                     `;
                 });
 
-                $('#usuarios').html(codigoHTML);
+                $('#users').html(codigoHTML);
             }
         });
     }
 
-    mostrarUsuarios();
+    showUsers();
 
     /* Nuevo Usuario */
-    $('#formNuevoUsuario').submit((e) => {
+    $('#formNewUser').submit((e) => {
         e.preventDefault();
-        console.log("hola");
         
-        var datos = {
-            nombre: $('#nuevoNombre').val(),
-            correo: $('#nuevoCorreo').val(),
-            password: $('#nuevoPassword').val(),
-            activo: true
+        var data = {
+            usr: $('#newUser').val(),
+            name: $('#newName').val(),
+            email: $('#newEmail').val(),
+            password: $('#newPassword').val()
         }
-        console.log(datos);
+        console.info(data);
         
-        $.post('http://localhost:3000/api/registrarUsuario2', datos, () => {
-            console.log("datos guardados");
-            $('#modalInsertarUsuario').modal('hide');
-            $('#nuevoNombre').val("");
-            $('#nuevoCorreo').val("");
-            $('#nuevoPassword').val("");
-            mostrarUsuarios();
+        $.post('http://localhost:3000/api/registrarUsuario2', data, () => { //TODO Cambiar Ruta de la API
+            console.info("Saved data");
+            $('#modalNewUser').modal('hide');
+            $('#newUser').val("");
+            $('#newName').val("");
+            $('#newEmail').val("");
+            $('#newPassword').val("");
+            showUsers();
         });
     });
 
@@ -58,54 +59,58 @@ $(document).ready( () => {
     /*  MODIFICAR CONTACTO  */
     $('body').on('click','.modificar', (e) => {
         e.preventDefault();
-        let idRegistro = $(e.target)[0].id;
-        console.info(idRegistro);
+        let idUser = $(e.target)[0].id;
+        console.info(idUser);
 
         $.ajax({
-            url: 'http://localhost:3000/api/editarUsuario/'+idRegistro,
+            url: 'http://localhost:3000/api/editarUsuario/'+idUser, //TODO Cambiar Ruta de la API
             type: 'GET',
-            success: (registro) => {
-                console.log(registro);
-                $('#editarUsuario').modal('show');
-                $('#editarId').val(registro.id);
-                $('#editarNombre').val(registro.nombre);
-                $('#editarCorreo').val(registro.correo);
-                $('#editarPassword').val(registro.password);
+            success: (user) => {
+                console.info(user);
+                $('#modalEditUser').modal('show');
+                $('#editId').val(user.id);
+                $('#editUser').val(user.usr);
+                $('#editName').val(user.name);
+                $('#editEmail').val(user.email);
+                $('#editPassword').val(user.password);
             }
         });
     });
 
     /*  MODIFICAR CONTACTO 2 */
-    $('#formEditarUsuario').submit((e) => {
+    $('#formEditUser').submit((e) => {
         e.preventDefault();
-        let datos = {
-            id: $('#editarId').val(),
-            nombre: $('#editarNombre').val(),
-            correo: $('#editarPassword').val()
+        let data = {
+            id_usr: $('#editId').val(),
+            usr: $('#editUser').val(),
+            name: $('#editName').val(),
+            email: $('#editEmail').val(),
+            password: $('#editPassword').val()
         }
 
-        $.post('http://localhost:3000/api/modificarUsuario2/',datos, (e) => {
-            $('#editarId').val('');
-            $('#editarNombre').val('');
-            $('#editarCorreo').val('');
-            $('#editarPassword').val('');
-            $('#editarUsuario').modal('hide');
-            mostrarUsuarios();
+        $.post('http://localhost:3000/api/modificarUsuario2/',datos, (e) => { //TODO Cambiar la Ruta de la API
+            $('#editId').val('');
+            $('#editUser').val('');
+            $('#editName').val('');
+            $('#editEmail').val('');
+            $('#editPassword').val('');
+            $('#modalEditUser').modal('hide');
+            showUsers();
         });
     });
 
     /*  BORRAR CONTACTO  */
     $('body').on('click','.borrar', (e) => {
         e.preventDefault();
-        let idRegistro = $(e.target)[0].id;
-        console.info(idRegistro);
+        let idUser = $(e.target)[0].id;
+        console.info(idUser);
 
         $.ajax({
-            url: 'http://localhost:3000/api/borrarUsuarioFisico/'+idRegistro,
+            url: 'http://localhost:3000/api/borrarUsuarioFisico/'+idUser, //TODO Cambiar la Ruta de la API
             type: 'GET',
-            success: (respuesta) => {
-                console.log(respuesta);
-                mostrarUsuarios();
+            success: (response) => {
+                console.info(response);
+                showUsers();
             }
         });
     });
